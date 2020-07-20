f222ffb8cf3d70f8ab051ef7c571c5ee9a4b230c
Vinícius Garcia <viniciusgarciatree@gmail.com>
2020-07-20 11:58:40 -0300

    Alteração para arquivos sistema e email não responda.

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'naoresponda.ieducar@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'i-Educar'),
     ],
 
